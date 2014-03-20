74b3a46eb9ef711a25a69407b1e0504ac73ce3cc
Justin Love <git@JustinLove.name>
2014-03-20 16:42:08 -0500

    wobbling sound

--- script/audio.js
+++ script/audio.js
@@ -1,6 +1,7 @@
 define(['jquery'], function($) {
   var context;
   var source;
+  var gain;
 
   var createContext = function() {
     var context
@@ -20,15 +21,26 @@ define(['jquery'], function($) {
 
   var start = function() {
     source = context.createOscillator()
-    source.frequency.value = 440
-    source.connect(context.destination)
+    source.frequency.setValueAtTime(440, context.currentTime)
+    wobble()
+    source.connect(gain)
+    //gain.gain.setValueAtTime(1.0, context.currentTime)
+    //gain.gain.exponentialRampToValueAtTime(0.1, context.currentTime + 5)
     source.start(0)
   }
 
   var stop = function() {
+    if (wobbler) clearTimeout(wobbler)
     source.stop(0)
     source.disconnect()
     source = null
+  }
+
+  var wobbler
+  var wobble = function() {
+    source.frequency.exponentialRampToValueAtTime(880, context.currentTime + 2)
+    source.frequency.exponentialRampToValueAtTime(440, context.currentTime + 4)
+    wobbler = setTimeout(wobble, 4*1000)
   }
 
   $('#play').on('change', function() {
@@ -42,6 +54,8 @@ define(['jquery'], function($) {
   return {
     ready: function() {
       context = createContext()
+      gain = context.createGain()
+      gain.connect(context.destination)
     }
   }
 })
