caf89a67c66f6ab507d742e5e74ebd065e7129db
Justin Love <git@JustinLove.name>
2014-03-24 11:15:15 -0500

    extract leap interface

--- script/audio.js
+++ script/audio.js
@@ -1,15 +1,6 @@
-define(['audiograph', 'jquery', 'leap'], function(audiograph, $) {
-  var controller
+define(['audiograph', 'hands', 'jquery'], function(audiograph, hands, $) {
   var stopAnimation = function() {}
   var stopPointing = function() {}
-
-  var right = {
-    div: '#right',
-  }
-
-  var left = {
-    div: '#left',
-  }
 
   var targets = [
     {
@@ -40,15 +31,15 @@ define(['audiograph', 'jquery', 'leap'], function(audiograph, $) {
 
   var start = function() {
     audiograph.start()
+    hands.start()
 
-    controller.connect()
     stopAnimation = animate()
     stopPointer = point()
   }
 
   var stop = function() {
     audiograph.stop()
-    controller.disconnect()
+    hands.stop()
     stopAnimation()
     stopPointing()
   }
@@ -56,15 +47,15 @@ define(['audiograph', 'jquery', 'leap'], function(audiograph, $) {
   var animate = function() {
     var timeout
     var round = function() {
-      processFrame(controller.frame())
+      hands.poll()
 
       //source.frequency.value = 2*rHeight
       //filter.frequency.value = Math.abs(rWidth)
       //filter.Q.value = rWidth/2
       //volume.gain.value = Math.sqrt(Math.max(rDepth, 0))
 
-      audiograph.delay.delayTime.value = left.width / -200
-      audiograph.delayGain.gain.value = left.height / 400
+      audiograph.delay.delayTime.value = hands.left.width / -200
+      audiograph.delayGain.gain.value = hands.left.height / 400
 
       timeout = setTimeout(round, 100)
     }
@@ -77,14 +68,13 @@ define(['audiograph', 'jquery', 'leap'], function(audiograph, $) {
   var point = function() {
     var run = true
     var round = function() {
-      processFrame(controller.frame())
+      hands.poll()
 
-      $('#pointer').css({left: right.x - 25, top: right.y - 25})
+      $('#pointer').css({left: hands.right.x - 25, top: hands.right.y - 25})
 
       targets.forEach(function(t) {
-        if (Math.abs(right.x - t.x) < t.size && Math.abs(right.y - t.y) < t.size) {
+        if (Math.abs(hands.right.x - t.x) < t.size && Math.abs(hands.right.y - t.y) < t.size) {
           if (!t.active) {
-            console.log('hit')
             //audiograph.note(100 + Math.random() * 550, 2)
             audiograph.note(t.frequency, 2)
             t.active = true
@@ -104,66 +94,6 @@ define(['audiograph', 'jquery', 'leap'], function(audiograph, $) {
     }
   }
 
-  var processFrame = function(frame) {
-    if (frame.hands.length < 1) return
-
-    frame.hands.forEach(function(hand) {
-      if (!right.id && hand.stabilizedPalmPosition[0] > 0) {
-        right.id = hand.id
-      }
-
-      if (!left.id && hand.stabilizedPalmPosition[0] < 0) {
-        left.id = hand.id
-      }
-    })
-
-    var data
-
-    if (data = frame.handsMap[right.id]) {
-      processHand(right, data)
-    } else {
-      right.id = null
-    }
-
-    if (data = frame.handsMap[left.id]) {
-      processHand(left, data)
-    } else {
-      left.id = null
-    }
-  }
-
-  var processHand = function(hand, data) {
-    var $div = $(hand.div)
-
-    //console.log(frame)
-    var s = data.stabilizedPalmPosition
-    //console.log(s)
-    hand.width = s[0]
-    hand.height = s[1] - 50
-    hand.depth = s[2]
-
-    $div.find('.id').text(hand.id)
-    $div.find('.width').text(hand.width)
-    $div.find('.height').text(hand.height)
-    $div.find('.depth').text(hand.depth)
-
-    if (data.pointables[0]) {
-      var t = data.pointables[0].stabilizedTipPosition
-      var d = data.pointables[0].direction
-
-      hand.x = t[0] + d[0]*700
-      hand.y = t[1] + d[1]*700
-
-      var width = $(document).width() / 2
-      var height = $(document).height()
-
-      hand.x = width + hand.x/300 * width
-      hand.y = height - hand.y/300 * height
-
-      $div.find('.x').text(right.x)
-      $div.find('.y').text(right.y)
-    }
-  }
 
   $('#play').on('change', function() {
     if ($(this).prop('checked')) {
@@ -177,7 +107,7 @@ define(['audiograph', 'jquery', 'leap'], function(audiograph, $) {
     ready: function() {
       audiograph.create()
 
-      controller = new Leap.Controller()
+      hands.create()
 
       targets.forEach(function(t) {
         $("<div class='target' id="+t.id+"></div>")
---
+++ script/hands.js
@@ -0,0 +1,90 @@
+define(['leap'], function() {
+  var controller
+
+  var right = {
+    div: '#right',
+  }
+
+  var left = {
+    div: '#left',
+  }
+
+  var processFrame = function(frame) {
+    if (frame.hands.length < 1) return
+
+    frame.hands.forEach(function(hand) {
+      if (!right.id && hand.stabilizedPalmPosition[0] > 0) {
+        right.id = hand.id
+      }
+
+      if (!left.id && hand.stabilizedPalmPosition[0] < 0) {
+        left.id = hand.id
+      }
+    })
+
+    var data
+
+    if (data = frame.handsMap[right.id]) {
+      processHand(right, data)
+    } else {
+      right.id = null
+    }
+
+    if (data = frame.handsMap[left.id]) {
+      processHand(left, data)
+    } else {
+      left.id = null
+    }
+  }
+
+  var processHand = function(hand, data) {
+    var $div = $(hand.div)
+
+    //console.log(frame)
+    var s = data.stabilizedPalmPosition
+    //console.log(s)
+    hand.width = s[0]
+    hand.height = s[1] - 50
+    hand.depth = s[2]
+
+    $div.find('.id').text(hand.id)
+    $div.find('.width').text(hand.width)
+    $div.find('.height').text(hand.height)
+    $div.find('.depth').text(hand.depth)
+
+    if (data.pointables[0]) {
+      var t = data.pointables[0].stabilizedTipPosition
+      var d = data.pointables[0].direction
+
+      hand.x = t[0] + d[0]*700
+      hand.y = t[1] + d[1]*700
+
+      var width = $(document).width() / 2
+      var height = $(document).height()
+
+      hand.x = width + hand.x/300 * width
+      hand.y = height - hand.y/300 * height
+
+      $div.find('.x').text(right.x)
+      $div.find('.y').text(right.y)
+    }
+  }
+
+
+  return {
+    right: right,
+    left: left,
+    create: function() {
+      controller = new Leap.Controller()
+    },
+    start: function() {
+      controller.connect()
+    },
+    stop: function() {
+      controller.disconnect()
+    },
+    poll: function() {
+      processFrame(controller.frame())
+    }
+  }
+})
